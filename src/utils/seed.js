e398e158a710cb9f7220705d8f0e6cc9cb35deba
Guille <guillempaniagua@gmail.com>
2026-04-27 17:15:14 +0000

    feat: seed de fighters iniciales

---
+++ src/utils/seed.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose')
+const connectDB = require('../config/db')
+const Fighter = require('../models/fighter.model')
+
+require('dotenv').config()
+
+const fighters = [
+  { name: 'Tyson Fury', weight: 120, country: 'UK' },
+  { name: 'Mike Tyson', weight: 100, country: 'USA' }
+]
+
+const seed = async () => {
+  try {
+    await connectDB()
+
+    await Fighter.deleteMany()
+    console.log('Fighters borrados')
+
+    await Fighter.insertMany(fighters)
+    console.log('Fighters insertados')
+
+    process.exit()
+  } catch (error) {
+    console.log(error)
+    process.exit(1)
+  }
+}
+
+seed()
